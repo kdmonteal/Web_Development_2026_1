a857e8d0eb97613c16336497ef809c48140343cf
Daniella Marin <kdmonteal@gmail.com>
2026-02-16 11:15:29 -0500

    js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
-alert('hola mundo');
+const x = document.getElementById("result");
+
+if (typeof(Storage) !== "undefined") {
+  x.innerHTML = "Your browser supports Web storage!";
+} else {
+  x.innerHTML = "Sorry, no Web storage support!";
+}
