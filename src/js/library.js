7fa3513388fefa74e742d9fb7a960ef66da0c05a
Daniella Marin <kdmonteal@gmail.com>
2026-02-23 11:19:07 -0500

    Library

---
+++ src/js/library.js
@@ -0,0 +1,11 @@
+// Prestamos del libro
+// Funcion que se llame procesar solicitud
+// procesarSoliciutud (name, libro1)
+
+// Console.log('Welcome Daniella - 11054879')
+
+
+// create dynamic array to see the selection
+function procesarSolicitud(userName, books) {
+    
+}
